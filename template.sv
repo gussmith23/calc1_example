66b74bf3d3beda00f42058e031c1c1b875ebefcc
gussmith23 <hfs5022@psu.edu>
2016-10-17 13:18:05 -0400

    adds clocking block

--- template.sv
+++ template.sv
@@ -15,20 +15,21 @@ interface calc_ifc(input bit clk);
   logic [1:7] reset;
   logic scan_in;
 
-  modport TEST_PROGRAM(
-    // Note: "input" and "output" keywords are sticky! We only need to use them
-    // once each.
+  // Now, we're using a clocking block in our interface.
+  clocking cb @(posedge clk);
     input out_data1, out_data2, out_data3, out_data4,
     out_resp1, out_resp2, out_resp3, out_resp4, 
-    scan_out, 
+    scan_out; 
 
     output error_found, 
     req1_cmd_in, req1_data_in, 
     req2_cmd_in, req2_data_in, 
     req3_cmd_in, req3_data_in,
     req4_cmd_in, req4_data_in, 
-    reset, 
-    scan_in);
+    scan_in;
+  endclocking
+
+  modport TEST_PROGRAM(clocking cb, output reset); 
     
 endinterface
 
@@ -37,37 +38,34 @@ endinterface
 program test(calc_ifc.TEST_PROGRAM calc_ifc1);
 
   initial begin 
-
-    // The calculator is expecting this initial reset and will not work without it.
     calc_ifc1.reset <= 7'b1111111;
-    #(7*`CLK_PERIOD) calc_ifc1.reset <= 0;// TODO: how can we do this better?
-                                          // what can we add in the interface so that
-                                          // we don't have to know anything about the
-                                          // clock period inside the test program?
+    repeat (7) @calc_ifc1.cb;
+    calc_ifc1.reset <= 0;
 
     // Input command. Command: addition. Arguments: 1 and 2.
-    calc_ifc1.req1_cmd_in <= 1;
-    calc_ifc1.req1_data_in <= 1;
-    #`CLK_PERIOD calc_ifc1.req1_cmd_in <= 0;
-    calc_ifc1.req1_data_in <= 2;
+    calc_ifc1.cb.req1_cmd_in <= 1;
+    calc_ifc1.cb.req1_data_in <= 1;
+    @calc_ifc1.cb;
+    calc_ifc1.cb.req1_cmd_in <= 0;
+    calc_ifc1.cb.req1_data_in <= 2;
 
     // Wait for the signal that the data line has data on it.
-    @ (calc_ifc1.out_resp1);
+    @ (calc_ifc1.cb.out_resp1);
 
     // Check that the calculator didn't return an error code.
-    assert(calc_ifc1.out_resp1 == 1) begin 
+    assert(calc_ifc1.cb.out_resp1 == 1) begin 
       $display("Response code is 1 - operation returned without error! Checking correctness...");
       // Check that the calculator calculated the addition correctly.
-      assert(calc_ifc1.out_data1 == 3) $display("Addition succeeded! Got 1 + 2 == 3.");
-      else $error("Addition failed! Got 1 + 2 == %d", calc_ifc1.out_data1);
+      assert(calc_ifc1.cb.out_data1 == 3) $display("Addition succeeded! Got 1 + 2 == 3.");
+      else $error("Addition failed! Got 1 + 2 == %d", calc_ifc1.cb.out_data1);
     end
-    else $error("Response code is %b - calculator error!", calc_ifc1.out_resp1);
+    else $error("Response code is %b - calculator error!", calc_ifc1.cb.out_resp1);
 
     $finish;
-
   end
 
 endprogram
+
 
 module testbench();
   
